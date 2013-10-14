38ce4d945a32c805b6dbcfb9b084824e4f8790b5
dbc-apprentice <apprentice@devbootcamp.com>
2013-10-14 16:57:05 -0500

    basic routing and game creation

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,4 +1,18 @@
 get '/' do
-  # Look in app/views/index.erb
   erb :index
+end
+
+get '/get_ur_racing_on' do
+  erb :game_board
+end
+
+post '/playercreate' do
+  @player1 = Player.find_or_create_by_name(name: params[:player1])
+  @player2 = Player.find_or_create_by_name(name: params[:player2])
+  @game = Game.create
+  @player1.games << @game
+  @player2.games << @game
+
+  round create?!
+  redirect '/get_ur_racing_on'
 end
--- app/models/player.rb
+++ app/models/player.rb
@@ -1,5 +1,5 @@
 class Player < ActiveRecord::Base
-  has_many :races,
+  has_many :races
   has_many :games, through: :races
   validates :name, uniqueness: true
 end
